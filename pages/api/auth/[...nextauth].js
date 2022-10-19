ba31794a7ce45397160265a66fb260e139e9b74e
dodadoa <maddodadoa@gmail.com>
2022-10-20 01:12:27 +0700

    fix NEXT_PUBLIC_SECRET

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,7 +14,7 @@ export default NextAuth({
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
