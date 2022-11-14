a6f15bdf7ccad812cff30164ad685bb0341df8d2
TropicalBoi <tewprai.bualoi@gmail.com>
2022-11-15 04:14:44 +0700

    Tew add new branch form with css

--- pages/form.js
+++ pages/form.js
@@ -1,52 +1,67 @@
-import React, { useEffect } from 'react'
-import { useForm } from 'react-hook-form'
-import { createRecord, tables } from '../external/airtable'
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { createRecord, tables } from "../external/airtable";
 import { getSession } from "next-auth/react";
-import { fetchAlbum } from '../external/spotify'
+import { fetchAlbum } from "../external/spotify";
+import style from "../styles/form.module.css";
 
 const Form = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
   const onSubmit = async (data) => {
     try {
       const fields = {
         caption: data.caption,
-        source: 'spotify',
-        target: 'any',
-        user: data.name
-      }
-      const result = await createRecord({ fields })
-      console.log(result)
+        source: "spotify",
+        target: "any",
+        user: data.name,
+      };
+      const result = await createRecord({ fields });
+      console.log(result);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const search = async (text) => {
     try {
-      const session = await getSession()
-      const accessToken = session.user.accessToken
-      const result = await fetchAlbum(accessToken)
-      console.log(result)
+      const session = await getSession();
+      const accessToken = session.user.accessToken;
+      const result = await fetchAlbum(accessToken);
+      console.log(result);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <input onChange={(e) => search(e.target.value)} />
-
-        <input  {...register("caption", { required: true })} />
-        {errors.caption && <span>This field is required</span>}
-
-        <input {...register("name", { required: true })} />
-        {errors.name && <span>This field is required</span>}
-
-        <input type="submit" />
-      </div>
-    </form>
+    <div className={style.form}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={style.formBody}>
+          <div>
+            <p>Search:</p>
+            <input onChange={(e) => search(e.target.value)} />
+          </div>
+          <div>
+            <p>Caption:</p>
+            <input {...register("caption", { required: true })} />
+            <p>{errors.caption && <span>This field is required!</span>}</p>
+          </div>
+          <div>
+            <p>Name:</p>
+            <input {...register("name", { required: true })} />
+            <p>{errors.name && <span>This field is required!</span>}</p>
+          </div>
+          <input type="submit" value="submit" />
+        </div>
+      </form>
+    </div>
   );
-}
+};
 
-export default Form
+export default Form;
