5aedec5bdf81017f0221a8fdc4957dcfa6490564
TropicalBoi <tewprai.bualoi@gmail.com>
2022-11-17 20:53:43 +0700

    add marquee caption

--- pages/index.js
+++ pages/index.js
@@ -1,167 +1,175 @@
-import { useEffect, useState, useCallback, useRef } from 'react'
-import { getSession } from "next-auth/react"
-import { isAuthenticated } from "../utils/isAuthenticated"
-import { tables, getRecord } from '../external/airtable'
+import { useEffect, useState, useCallback, useRef } from "react";
+import { getSession } from "next-auth/react";
+import { isAuthenticated } from "../utils/isAuthenticated";
+import { tables, getRecord } from "../external/airtable";
 import style from "../styles/index.module.css";
-import { addSongToQueue } from '../external/spotify'
-import { randomCountableNumber } from '../utils/random'
+import { addSongToQueue } from "../external/spotify";
+import { randomCountableNumber } from "../utils/random";
 
 //                    1s  * 60 = minute
 //                          1m * 4 = 4 minute
-const THREE_MINUTES = 1000 * 60 * 3
+const THREE_MINUTES = 1000 * 60 * 3;
 
 export default function Home({ hello }) {
-
-  const [player, setPlayer] = useState(null)
-  const [changing, setChanging] = useState(0)
-  const [isPaused, setPaused] = useState(false)
-  const [isActive, setActive] = useState(false)
-  const [activeSession, setActiveSession] = useState({})
-  const [recordSongDetail, setRecordSongDetail] = useState({ fields: { caption: '', user: '' }})
+  const [player, setPlayer] = useState(null);
+  const [changing, setChanging] = useState(0);
+  const [isPaused, setPaused] = useState(false);
+  const [isActive, setActive] = useState(false);
+  const [activeSession, setActiveSession] = useState({});
+  const [recordSongDetail, setRecordSongDetail] = useState({
+    fields: { caption: "", user: "" },
+  });
 
   const videoRef = useRef();
 
   const track = {
     name: "",
     album: {
-      images: [
-        { url: "" }
-      ]
+      images: [{ url: "" }],
     },
-    artists: [
-      { name: "" }
-    ]
-  }
-  const [currentTrack, setTrack] = useState(track)
+    artists: [{ name: "" }],
+  };
+  const [currentTrack, setTrack] = useState(track);
 
   const fetchSession = async () => {
-    const session = await getSession()
+    const session = await getSession();
 
-    setActiveSession(session)
-    const accessToken = session.user.accessToken
+    setActiveSession(session);
+    const accessToken = session.user.accessToken;
 
-    const script = document.createElement("script")
-    script.src = "https://sdk.scdn.co/spotify-player.js"
-    script.async = true
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
 
-    document.body.appendChild(script)
+    document.body.appendChild(script);
 
     window.onSpotifyWebPlaybackSDKReady = () => {
-
       const player = new window.Spotify.Player({
-        name: 'Imagining 1',
-        getOAuthToken: cb => { cb(accessToken) },
-        volume: 0.5
-      })
+        name: "Imagining 1",
+        getOAuthToken: (cb) => {
+          cb(accessToken);
+        },
+        volume: 0.5,
+      });
 
-      setPlayer(player)
+      setPlayer(player);
 
-      player.addListener('ready', ({ device_id }) => {
-        console.log('Ready with Device ID', device_id)
-      })
+      player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
 
-      player.addListener('not_ready', ({ device_id }) => {
-        console.log('Device ID has gone offline', device_id)
-      })
+      player.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
 
-      player.addListener('player_state_changed', (state => {
-
+      player.addListener("player_state_changed", (state) => {
         if (!state) {
-          return
+          return;
         }
-  
-        setTrack(state.track_window.current_track)
+
+        setTrack(state.track_window.current_track);
 
         getRecord(state.track_window.current_track.id)
           .then((result) => {
-            setRecordSongDetail(result)
+            setRecordSongDetail(result);
           })
           .catch((err) => {
-            console.log(err)
-          })
+            console.log(err);
+          });
 
-        setPaused(state.paused)
-  
-        player.getCurrentState().then(state => {
-          (!state) ? setActive(false) : setActive(true)
-        })
-  
-      }))
+        setPaused(state.paused);
 
-      player.connect()
-    }
-  }
+        player.getCurrentState().then((state) => {
+          !state ? setActive(false) : setActive(true);
+        });
+      });
+
+      player.connect();
+    };
+  };
 
   const fetchTablesAndRandomOneSong = async () => {
     try {
-      const tableData = await tables()
-      const { records } = tableData.data
+      const tableData = await tables();
+      const { records } = tableData.data;
       if (!records) {
-        console.log('Error, no Records')
-        return
+        console.log("Error, no Records");
+        return;
       }
-      const recordsLength = records.length
-      const randomNum = randomCountableNumber(recordsLength)
-      const pickedRecord = records[randomNum]
-      console.log(pickedRecord)
+      const recordsLength = records.length;
+      const randomNum = randomCountableNumber(recordsLength);
+      const pickedRecord = records[randomNum];
+      console.log(pickedRecord);
 
-      if (!activeSession && !activeSession.user && !activeSession.user.accessToken) {
-        console.log('Error, no active session')
-        return
+      if (
+        !activeSession &&
+        !activeSession.user &&
+        !activeSession.user.accessToken
+      ) {
+        console.log("Error, no active session");
+        return;
       }
 
-      const result = await addSongToQueue(activeSession.user.accessToken, pickedRecord.fields.songId)
-      console.log('Added song to queue!', result)
-
+      const result = await addSongToQueue(
+        activeSession.user.accessToken,
+        pickedRecord.fields.songId
+      );
+      console.log("Added song to queue!", result);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const refresh = async () => {
-    await fetchTablesAndRandomOneSong()
-    setTimeout(refresh, THREE_MINUTES)
-  }
+    await fetchTablesAndRandomOneSong();
+    setTimeout(refresh, THREE_MINUTES);
+  };
 
   useEffect(() => {
-    fetchSession()
-  }, [])
+    fetchSession();
+  }, []);
 
   useEffect(() => {
-    setTimeout(refresh, 100)
-  }, [])
+    setTimeout(refresh, 100);
+  }, []);
 
   useEffect(() => {
-    setTimeout(()=>{
-      videoRef.current.play()
-    },1000)
-  }, [])
+    setTimeout(() => {
+      videoRef.current.play();
+    }, 1000);
+  }, []);
 
   return (
     <div className={style.main}>
       <video loop ref={videoRef} muted className={style.video}>
-        <source src="/glitch_bg_1.mp4" type="video/mp4"/>
+        <source src="/glitch_bg_1.mp4" type="video/mp4" />
       </video>
 
       <div className={style.textDescriptionBox}>
         <div className={style.textDescription}>
           <p>Song Name: {currentTrack.name}</p>
-          <p>Artist: {currentTrack.artists.map((artist) => `${artist.name} `)}</p>
-          {recordSongDetail && <p>Suggested by: {recordSongDetail.fields.user}</p>}
+          <p>
+            Artist: {currentTrack.artists.map((artist) => `${artist.name} `)}
+          </p>
+          {recordSongDetail && (
+            <p>Suggested by: {recordSongDetail.fields.user}</p>
+          )}
         </div>
       </div>
-
+      <div className={style.captionMarquee}>
+        <p className={style.captionInner}>{recordSongDetail.fields.caption}</p>
+      </div>
       {/* <button onClick={() => {
         player.togglePlay()
         player.activateElement()
       }}> PLAY </button>
       <button onClick={handleNext}> NEXT </button> */}
     </div>
-  )
+  );
 }
 
 export const getServerSideProps = async (ctx) => {
-  const session = await getSession(ctx)
+  const session = await getSession(ctx);
 
   if (!(await isAuthenticated(session))) {
     return {
@@ -169,8 +177,8 @@ export const getServerSideProps = async (ctx) => {
         destination: "/login",
         permanent: false,
       },
-    }
+    };
   }
 
-  return { props: { hello: 'hello' } }
-}
+  return { props: { hello: "hello" } };
+};
